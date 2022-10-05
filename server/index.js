7f26b720c0242660538faabfffbd225a931f2538
mukhulaazam <mukhulaazam@gmail.com>
2022-10-05 10:08:27 +0600

    Client schema ready but data not response

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
-import express from "express";
+const express = require('express');
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+
+// @des :: Custom import files
+const schema = require('./schema/schema')
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// @des :: initilize GraphQl Entry points
+app.use('/graphql', graphqlHTTP({
+    // schema: DrMamun
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+}));
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`))
 
